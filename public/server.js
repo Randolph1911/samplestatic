ea15c3eec2247949578ad2992133bd60144d41a5
Randolph1911 <williamrandolph1911@gmail.com>
2019-01-17 19:42:13 -0500

    files for heroku

---
+++ public/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express'),
+serveStatic = require('serve-static'),
+path = require('path');
+
+// Instantiate the Express App
+const app = express();
+
+// create Middleware to handle the serving of the app
+app.use('/', serveStatic(path.join(__dirname, '/public')));
+
+// Serve Index by default
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/public/index.html');
+});
+
+// create default Port to serve the app on
+const port = process.env.PORT || 5000;
+
+// Start Server
+app.listen(port, () => {
+
+// Log to feedback that this code is actually running
+console.log(`Server start on port ${port}...`);
+});
