86a29871637fb74704f1c629066d96268968b8ea
Randolph1911 <williamrandolph1911@gmail.com>
2019-01-17 20:10:07 -0500

    altered server.js code to be identical to syntax in tutorial

--- public/server.js
+++ public/server.js
@@ -1,3 +1,28 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+// create the Express App
+const app = express()
+
+// create Middleware to handle the serving of the app
+app.use("/", serveStatic(path.join(__dirname, '/public')));
+
+// Serve Index by default
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// create default Port to serve the app on
+const port = process.env.PORT || 5000
+
+// Start Server
+app.listen(port)
+
+// Log to feedback that this code is actually running
+console.log('Server start on port' + port)
+
+/*
 'use strict';
 
 const express = require('express'),
@@ -24,3 +49,4 @@ app.listen(port, () => {
 // Log to feedback that this code is actually running
 console.log(`Server start on port ${port}...`);
 });
+*/
